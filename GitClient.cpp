1719afc241463c55960f4a048006bac39674a402
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-04-30 22:31:19 +0300

    GitClient.cpp - isGit, fix

--- GitClient.cpp
+++ GitClient.cpp
@@ -41,8 +41,15 @@ GitClient::isGit() const
     std::ctstring_t                     filePath = "/usr/bin/git";
     std::cvec_tstring_t                 params   = {"rev-parse", "--git-dir"};
     const std::set<std::pair_tstring_t> envs;
+	std::tstring_t                      stdOut;
+	std::tstring_t                      stdError;
 
-	Process::execute(filePath, xTIMEOUT_INFINITE, params, envs);
+	Process::execute(filePath, xTIMEOUT_INFINITE, params, envs, &stdOut, &stdError);
+	// Cout() << xTRACE_VAR(stdOut);
+	// Cout() << xTRACE_VAR(stdError);
+
+	xCHECK_RET(!stdError.empty(), false);
+	xCHECK_RET((stdOut.find(xT(".git")) == std::tstring_t::npos), false);
 
     return true;
 }
