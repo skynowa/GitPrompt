3b733ca470f73c3edbd2c53bb78dbd31dceccae5
skynowa <skynowa@gmail.com>
2023-01-09 23:32:42 +0200

    Config.h [4-] - namespaces - rm

--- Config.h
+++ Config.h
@@ -7,10 +7,6 @@
 #pragma once
 
 using namespace xl;
-using namespace xl::debug;
-using namespace xl::fs;
-using namespace xl::sync;
-using namespace xl::system;
 //-------------------------------------------------------------------------------------------------
 const std::ctstring_t appName = xT("GitPrompt");
 //-------------------------------------------------------------------------------------------------
