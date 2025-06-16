c25f21bb3f34ecd002cb7dafc132b5c9ac2f4bcd
skynowa <skynowa@gmail.com>
2025-06-16 16:41:14 +0300

    Config.h (+2) [27+/21-] - Config - review

--- Config.h
+++ Config.h
@@ -10,3 +10,30 @@ using namespace xl;
 //-------------------------------------------------------------------------------------------------
 inline const std::ctstring_t appName = xT("GitPrompt");
 //-------------------------------------------------------------------------------------------------
+namespace git_prompt
+{
+
+struct Config
+{
+
+	// Host
+	bool_t             isHostName      {false};
+	std::vec_tstring_t myHostNames     {xT("skynowa-pc"), xT("skynowa-laptop")};
+
+	// User
+	bool_t             isUserName      {false};
+	std::vec_tstring_t myUserNames     {xT("skynowa")};
+
+	// Dirs
+	bool_t             isDirPathInGitRepo {false};
+		///< Don't show current dir path in Git repository
+
+	// Volume
+	bool_t             isVolumeUsedPct {true};	///< force using VolumeUsedPct
+
+	// Power supply
+	bool_t             isPowerSupply   {true};	///< force using power supply info
+};
+
+} // namespace
+//-------------------------------------------------------------------------------------------------
