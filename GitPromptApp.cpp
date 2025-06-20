835d24f3f5bc1054002985c41a85ff28e94ddf5b
skynowa <skynowa@gmail.com>
2025-06-20 11:53:04 +0300

    xlib-update: GitPromptApp.cpp [92+/83-] - update

--- GitPromptApp.cpp
+++ GitPromptApp.cpp
@@ -63,7 +63,282 @@ GitPromptApp::onRun() /* final */
 		volumeUsedPct = (total - available) * 100 / total;
 	}
 
-	Console console(true, true);
+	std::tstring_t ps1;
+
+	// Color
+	cbool_t isColorSupport {true};
+	cbool_t isEscapeValues {false};
+
+	// Foreground
+	constexpr auto fgGreen   = Color::FG::Green;
+	constexpr auto fgYellow  = Color::FG::Yellow;
+	constexpr auto fgBlue    = Color::FG::Blue;
+	constexpr auto fgMagenta = Color::FG::Magenta;
+	constexpr auto fgRed     = Color::FG::Red;
+	constexpr auto fgWhite   = Color::FG::White;
+	constexpr auto fgCyan    = Color::FG::Cyan;
+	constexpr auto fgDefault = Color::FG::Default;
+
+	// Background
+	constexpr auto bgDefault = Color::BG::Default;
+
+	// Attribute
+	constexpr auto attrBold  = Color::Attr::Bold;
+
+	// Current date
+	{
+		// [
+		{
+			std::ctstring_t &str = xT("[");
+		#if 0
+			ps1 += console.setAttrsText(fgWhite, bgDefault, attrBold).setText(str);
+		#else
+			ps1 += Color(isColorSupport, isEscapeValues, fgWhite, bgDefault, attrBold).setText(str);
+		#endif
+		}
+
+		// Current date
+		{
+			std::ctstring_t &str = DateTime().current().format(xT("%d-%h %H:%M"), {});
+			ps1 += Color(isColorSupport, isEscapeValues, fgMagenta, bgDefault, attrBold).setText(str);
+		}
+
+		// ]
+		{
+			std::ctstring_t &str = xT("]");
+			ps1 += Color(isColorSupport, isEscapeValues, fgWhite, bgDefault, attrBold).setText(str);
+		}
+	}
+
+	// Shell last error
+	{
+	   /**
+		* ps1 += "$(if [[ $? == 0 ]]; then
+		*     echo \"\\[\033[0;32m\\]✔\";
+		* else
+		*     echo \"\\[\033[0;31m\\]✖\";
+		* fi)\\[\033[00m\\]";
+		*/
+
+		std::tstring_t lastShellOk;
+		{
+			std::ctstring_t &str = xT("✔");
+			lastShellOk = Color(isColorSupport, isEscapeValues, fgGreen, bgDefault, attrBold).setText(str);
+		}
+
+		std::tstring_t lastShellError;
+		{
+			std::ctstring_t &str = xT("✖");
+			lastShellError = Color(isColorSupport, isEscapeValues, fgRed, bgDefault, attrBold).setText(str);
+		}
+
+		ps1 += Format::str(xT("$(if [[ $? == 0 ]]; then echo \"{}\"; else echo \"{}\"; fi)"),
+			lastShellOk, lastShellError);
+	}
+
+	// User name
+	if (_config.isUserName ||
+		!Algos::isContains(_config.myUserNames, loginName))
+	{
+		std::ctstring_t &str = loginName;
+		const auto       fg  = isAdmin ? fgRed : fgMagenta;
+
+		ps1 += Color(isColorSupport, isEscapeValues, fg, bgDefault, attrBold).setText(str);
+	}
+
+	// Host name
+	if (_config.isHostName ||
+		!Algos::isContains(_config.myHostNames, hostName))
+	{
+		std::ctstring_t &str = hostName;
+		const auto       fg  = fgCyan;
+
+		ps1 += xT("@");
+		ps1 += Color(isColorSupport, isEscapeValues, fg, bgDefault, attrBold).setText(str);
+	}
+
+	ps1 += xT(" ");
+
+	// Git repository
+	if (isGitDir) {
+		std::ctstring_t &gitRepoName = git.repoName();
+
+		std::vec_tstring_t remoteRepoNames;
+		git.remoteRepoNames(&remoteRepoNames);
+
+		// [
+		{
+			std::ctstring_t &str = xT("[");
+			ps1 += Color(isColorSupport, isEscapeValues, fgWhite, bgDefault, attrBold).setText(str);
+		}
+
+		// Git repo URL name, remote repository names
+		{
+			std::ctstring_t &str = Format::str(xT("{}*{}"), git.repoUrlName(), remoteRepoNames.size());
+			ps1 += Color(isColorSupport, isEscapeValues, fgBlue, bgDefault, attrBold).setText(str);
+
+			std::ctstring_t &sep = xT("/");
+			ps1 += Color(isColorSupport, isEscapeValues, fgDefault, bgDefault, attrBold).setText(sep);
+		}
+
+		// Gitlab repo group name
+		if (std::ctstring_t &groupName = git.gitlabRepoGroupName();
+			!groupName.empty())
+		{
+			std::ctstring_t &str = Format::str(xT("{}"), groupName);
+			ps1 += Color(isColorSupport, isEscapeValues, fgCyan, bgDefault, attrBold).setText(str);
+
+			std::ctstring_t &sep = xT("/");
+			ps1 += Color(isColorSupport, isEscapeValues, fgDefault, bgDefault, attrBold).setText(sep);
+		}
+
+		// Git repository name
+		{
+			std::ctstring_t &str = Format::str(xT("{}"), gitRepoName);
+			ps1 += Color(isColorSupport, isEscapeValues, fgYellow, bgDefault, attrBold).setText(str);
+		}
+
+		// ]
+		{
+			std::ctstring_t &str = xT("]");
+			ps1 += Color(isColorSupport, isEscapeValues, fgWhite, bgDefault, attrBold).setText(str);
+		}
+
+		if ( !gitRepoName.empty() ) {
+			ps1 += xT(" ");
+		}
+	}
+
+	// Current dir
+	if (isGitDir) {
+		if (_config.isDirPathInGitRepo) {
+			std::ctstring_t &str = currentDirPathBrief;
+			ps1 += Color(isColorSupport, isEscapeValues, fgGreen, bgDefault, attrBold).setText(str);
+		}
+	} else {
+		std::ctstring_t &str = currentDirPathBrief;
+		ps1 += Color(isColorSupport, isEscapeValues, fgGreen, bgDefault, attrBold).setText(str);
+	}
+
+	// Volume used %
+	if (_config.isVolumeUsedPct ||
+		volumeUsedPct > ::volumeUsedWarnPct)
+	{
+		if (ps1.back() != Const::space()[0]) {
+			ps1 += xT(" ");
+		}
+
+		std::ctstring_t &str = Format::str(xT("{}%"), volumeUsedPct);
+		ps1 += Color(isColorSupport, isEscapeValues, fgWhite, bgDefault, attrBold).setText(str);
+	}
+
+	if (isGitDir) {
+		ps1 += xT(" ");
+
+		// Git branch name
+		{
+			// [
+			{
+				std::ctstring_t &str = xT("[");
+				ps1 += Color(isColorSupport, isEscapeValues, fgWhite, bgDefault, attrBold).setText(str);
+			}
+
+			// Git branch name
+			{
+				std::ctstring_t &str = git.branchName();
+				ps1 += Color(isColorSupport, isEscapeValues, fgRed, bgDefault, attrBold).setText(str);
+			}
+
+			// ]
+			{
+				std::ctstring_t &str = xT("]");
+				ps1 += Color(isColorSupport, isEscapeValues, fgWhite, bgDefault, attrBold).setText(str);
+			}
+		}
+
+		// Local branches number
+		{
+			std::csize_t localBranchesNum = git.localBranchesNum();
+			if (localBranchesNum > 0) {
+				std::ctstring_t &str = Format::str(xT("⎇{}"), localBranchesNum);
+				ps1 += Color(isColorSupport, isEscapeValues, fgWhite, bgDefault, attrBold).setText(str);
+			}
+		}
+
+		// Git file statuses
+		{
+			std::ctstring_t &str = git.filesStatuses();
+			ps1 += Color(isColorSupport, isEscapeValues, fgYellow, bgDefault, attrBold).setText(str);
+		}
+
+		// Git files number
+		{
+			std::cvec_tstring_t filterFileExts {};
+			std::vec_tstring_t  filePathes;
+			git.modifiedFiles(filterFileExts, &filePathes);
+
+			std::ctstring_t &filesNum = std::to_string( filePathes.size() );
+			if (filesNum != xT("0")) {
+				ps1 += Color(isColorSupport, isEscapeValues, fgYellow, bgDefault, attrBold).setText(filesNum);
+			}
+		}
+
+		// Git ahead/behind commits
+		{
+			std::tstring_t str;
+			{
+				std::size_t aheadNum  {};
+				std::size_t behindNum {};
+				git.commitsAheadBehind(&aheadNum, &behindNum);
+
+				if (aheadNum != 0) {
+					str = Format::str(xT("↑{}"), aheadNum);
+				}
+
+				if (behindNum != 0) {
+					str = Format::str(xT("↓{}"), behindNum);
+				}
+			}
+
+			ps1 += Color(isColorSupport, isEscapeValues, fgMagenta, bgDefault, attrBold).setText(str);
+		}
+
+		// Stashes number
+		{
+			std::csize_t stashesNum = git.stashesNum();
+			if (stashesNum > 0) {
+				std::ctstring_t &str = Format::str(xT("⚑{}"), stashesNum);
+				ps1 += Color(isColorSupport, isEscapeValues, fgBlue, bgDefault, attrBold).setText(str);
+			}
+		}
+	} // if (isGitDir)
+
+	// Is admin user
+	{
+		std::ctstring_t &str = isAdmin ? xT("#") : xT("$");
+		const auto       fg  = isAdmin ? fgRed : fgDefault;
+
+		ps1 += xT(" ");
+		ps1 += Color(isColorSupport, isEscapeValues, fg, bgDefault, attrBold).setText(str);
+		ps1 += xT(" ");
+	}
+
+	// mc
+	if (isMc) {
+		std::ctstring_t str = xT("mc");
+		const auto      fg  = fgCyan;
+
+		ps1 += Color(isColorSupport, isEscapeValues, fg, bgDefault, attrBold).setText(str);
+		ps1 += xT(" ");
+	}
+
+	// "> "
+	{
+		std::ctstring_t &str = xT("❱ ");
+		ps1 += Color(isColorSupport, isEscapeValues, fgYellow, bgDefault, attrBold).setText(str);
+	}
+
+	// Console
 	{
 		info::PowerSupply powerSupplyInfo;
 		info::Os          osInfo;
@@ -104,277 +379,11 @@ GitPromptApp::onRun() /* final */
 		std::ctstring_t &title = Format::str(xT("{}@{} - {}, {}, CPUs: {}, VPN: {}{}                Build: {}"),
 			hostName, loginName, osInfo.distro(), osInfo.desktopName(), cpuInfo.num(), isVpn,
 			powerSupply, BuildInfo().datetime());
+
+		Console console;
+		console.writeLine(ps1);
 		console.setTitle(title);
 	}
-
-	std::tstring_t ps1;
-
-	// Foreground
-	constexpr auto fgGreen   = Console::FG::Green;
-	constexpr auto fgYellow  = Console::FG::Yellow;
-	constexpr auto fgBlue    = Console::FG::Blue;
-	constexpr auto fgMagenta = Console::FG::Magenta;
-	constexpr auto fgRed     = Console::FG::Red;
-	constexpr auto fgWhite   = Console::FG::White;
-	constexpr auto fgCyan    = Console::FG::Cyan;
-	constexpr auto fgDefault = Console::FG::Default;
-
-	// Background
-	constexpr auto bgDefault = Console::BG::Default;
-
-	// Attribute
-	constexpr auto attrBold  = Console::Attr::Bold;
-
-	// Current date
-	{
-		// [
-		{
-			std::ctstring_t &str = xT("[");
-			ps1 += console.setAttrsText(fgWhite, bgDefault, attrBold, str);
-		}
-
-		// Current date
-		{
-			std::ctstring_t &str = DateTime().current().format(xT("%d-%h %H:%M"), {});
-			ps1 += console.setAttrsText(fgMagenta, bgDefault, attrBold, str);
-		}
-
-		// ]
-		{
-			std::ctstring_t &str = xT("]");
-			ps1 += console.setAttrsText(fgWhite, bgDefault, attrBold, str);
-		}
-	}
-
-	// Shell last error
-	{
-	   /**
-		* ps1 += "$(if [[ $? == 0 ]]; then
-		*     echo \"\\[\033[0;32m\\]✔\";
-		* else
-		*     echo \"\\[\033[0;31m\\]✖\";
-		* fi)\\[\033[00m\\]";
-		*/
-
-		std::tstring_t lastShellOk;
-		{
-			std::ctstring_t &str = xT("✔");
-			lastShellOk = console.setAttrsText(fgGreen, bgDefault, attrBold, str);
-		}
-
-		std::tstring_t lastShellError;
-		{
-			std::ctstring_t &str = xT("✖");
-			lastShellError = console.setAttrsText(fgRed, bgDefault, attrBold, str);
-		}
-
-		ps1 += Format::str(xT("$(if [[ $? == 0 ]]; then echo \"{}\"; else echo \"{}\"; fi)"),
-			lastShellOk, lastShellError);
-	}
-
-	// User name
-	if (_config.isUserName ||
-		!Algos::isContains(_config.myUserNames, loginName))
-	{
-		std::ctstring_t &str = loginName;
-		const auto       fg  = isAdmin ? fgRed : fgMagenta;
-
-		ps1 += console.setAttrsText(fg, bgDefault, attrBold, str);
-	}
-
-	// Host name
-	if (_config.isHostName ||
-		!Algos::isContains(_config.myHostNames, hostName))
-	{
-		std::ctstring_t &str = hostName;
-		const auto       fg  = fgCyan;
-
-		ps1 += xT("@");
-		ps1 += console.setAttrsText(fg, bgDefault, attrBold, str);
-	}
-
-	ps1 += xT(" ");
-
-	// Git repository
-	if (isGitDir) {
-		std::ctstring_t &gitRepoName = git.repoName();
-
-		std::vec_tstring_t remoteRepoNames;
-		git.remoteRepoNames(&remoteRepoNames);
-
-		// [
-		{
-			std::ctstring_t &str = xT("[");
-			ps1 += console.setAttrsText(fgWhite, bgDefault, attrBold, str);
-		}
-
-		// Git repo URL name, remote repository names
-		{
-			std::ctstring_t &str = Format::str(xT("{}*{}"), git.repoUrlName(), remoteRepoNames.size());
-			ps1 += console.setAttrsText(fgBlue, bgDefault, attrBold, str);
-
-			std::ctstring_t &sep = xT("/");
-			ps1 += console.setAttrsText(fgDefault, bgDefault, attrBold, sep);
-		}
-
-		// Gitlab repo group name
-		if (std::ctstring_t &groupName = git.gitlabRepoGroupName();
-			!groupName.empty())
-		{
-			std::ctstring_t &str = Format::str(xT("{}"), groupName);
-			ps1 += console.setAttrsText(fgCyan, bgDefault, attrBold, str);
-
-			std::ctstring_t &sep = xT("/");
-			ps1 += console.setAttrsText(fgDefault, bgDefault, attrBold, sep);
-		}
-
-		// Git repository name
-		{
-			std::ctstring_t &str = Format::str(xT("{}"), gitRepoName);
-			ps1 += console.setAttrsText(fgYellow, bgDefault, attrBold, str);
-		}
-
-		// ]
-		{
-			std::ctstring_t &str = xT("]");
-			ps1 += console.setAttrsText(fgWhite, bgDefault, attrBold, str);
-		}
-
-		if ( !gitRepoName.empty() ) {
-			ps1 += xT(" ");
-		}
-	}
-
-	// Current dir
-	if (isGitDir) {
-		if (_config.isDirPathInGitRepo) {
-			std::ctstring_t &str = currentDirPathBrief;
-			ps1 += console.setAttrsText(fgGreen, bgDefault, attrBold, str);
-		}
-	} else {
-		std::ctstring_t &str = currentDirPathBrief;
-		ps1 += console.setAttrsText(fgGreen, bgDefault, attrBold, str);
-	}
-
-	// Volume used %
-	if (_config.isVolumeUsedPct ||
-		volumeUsedPct > ::volumeUsedWarnPct)
-	{
-		if (ps1.back() != Const::space()[0]) {
-			ps1 += xT(" ");
-		}
-
-		std::ctstring_t &str = Format::str(xT("{}%"), volumeUsedPct);
-		ps1 += console.setAttrsText(fgWhite, bgDefault, attrBold, str);
-	}
-
-	if (isGitDir) {
-		ps1 += xT(" ");
-
-		// Git branch name
-		{
-			// [
-			{
-				std::ctstring_t &str = xT("[");
-				ps1 += console.setAttrsText(fgWhite, bgDefault, attrBold, str);
-			}
-
-			// Git branch name
-			{
-				std::ctstring_t &str = git.branchName();
-				ps1 += console.setAttrsText(fgRed, bgDefault, attrBold, str);
-			}
-
-			// ]
-			{
-				std::ctstring_t &str = xT("]");
-				ps1 += console.setAttrsText(fgWhite, bgDefault, attrBold, str);
-			}
-		}
-
-		// Local branches number
-		{
-			std::csize_t localBranchesNum = git.localBranchesNum();
-			if (localBranchesNum > 0) {
-				std::ctstring_t &str = Format::str(xT("⎇{}"), localBranchesNum);
-				ps1 += console.setAttrsText(fgWhite, bgDefault, attrBold, str);
-			}
-		}
-
-		// Git file statuses
-		{
-			std::ctstring_t &str = git.filesStatuses();
-			ps1 += console.setAttrsText(fgYellow, bgDefault, attrBold, str);
-		}
-
-		// Git files number
-		{
-			std::cvec_tstring_t filterFileExts {};
-			std::vec_tstring_t  filePathes;
-			git.modifiedFiles(filterFileExts, &filePathes);
-
-			std::ctstring_t &filesNum = std::to_string( filePathes.size() );
-			if (filesNum != xT("0")) {
-				ps1 += console.setAttrsText(fgYellow, bgDefault, attrBold, filesNum);
-			}
-		}
-
-		// Git ahead/behind commits
-		{
-			std::tstring_t str;
-			{
-				std::size_t aheadNum  {};
-				std::size_t behindNum {};
-				git.commitsAheadBehind(&aheadNum, &behindNum);
-
-				if (aheadNum != 0) {
-					str = Format::str(xT("↑{}"), aheadNum);
-				}
-
-				if (behindNum != 0) {
-					str = Format::str(xT("↓{}"), behindNum);
-				}
-			}
-
-			ps1 += console.setAttrsText(fgMagenta, bgDefault, attrBold, str);
-		}
-
-		// Stashes number
-		{
-			std::csize_t stashesNum = git.stashesNum();
-			if (stashesNum > 0) {
-				std::ctstring_t &str = Format::str(xT("⚑{}"), stashesNum);
-				ps1 += console.setAttrsText(fgBlue, bgDefault, attrBold, str);
-			}
-		}
-	} // if (isGitDir)
-
-	// Is admin user
-	{
-		std::ctstring_t &str = isAdmin ? xT("#") : xT("$");
-		const auto       fg  = isAdmin ? fgRed : fgDefault;
-
-		ps1 += xT(" ");
-		ps1 += console.setAttrsText(fg, bgDefault, attrBold, str);
-		ps1 += xT(" ");
-	}
-
-	// mc
-	if (isMc) {
-		std::ctstring_t str = xT("mc");
-		const auto      fg  = fgCyan;
-
-		ps1 += console.setAttrsText(fg, bgDefault, attrBold, str);
-		ps1 += xT(" ");
-	}
-
-	// "> "
-	{
-		std::ctstring_t &str = xT("❱ ");
-		ps1 += console.setAttrsText(fgYellow, bgDefault, attrBold, str);
-	}
-
-	console.writeLine(ps1);
 
 	return ExitCode::Success;
 }
