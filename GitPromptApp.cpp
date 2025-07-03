cc37d4f75f8d9ed3cc8b713b7c66234b7dade082
skynowa <skynowa@gmail.com>
2025-07-03 12:47:01 +0300

    console-color: GitPromptApp.cpp [30+/2-] - impl

--- GitPromptApp.cpp
+++ GitPromptApp.cpp
@@ -17,6 +17,34 @@ constexpr ulonglong_t volumeUsedWarnPct {90};
 constexpr std::size_t leftDirsNum       {2};
 constexpr std::size_t rightDirsNum      {2};
 
+std::string wrapAnsiForPS1(const std::string& input)
+{
+    static const std::regex ansiRegex("\x1B\\[[0-9;?]*[A-Za-z]");
+    std::string output;
+    std::sregex_iterator it(input.begin(), input.end(), ansiRegex);
+    std::sregex_iterator end;
+
+    size_t lastPos = 0;
+    for (; it != end; ++it) {
+        auto match = *it;
+        size_t start = match.position();
+        size_t length = match.length();
+
+        // Append text before the match
+        output.append(input, lastPos, start - lastPos);
+        // Append the wrapped ANSI sequence
+        output.append("\\[");
+        output.append(match.str());
+        output.append("\\]");
+
+        lastPos = start + length;
+    }
+    // Append remaining text
+    output.append(input, lastPos, std::string::npos);
+
+    return output;
+}
+
 }
 //-------------------------------------------------------------------------------------------------
 
@@ -382,8 +410,8 @@ GitPromptApp::onRun() /* final */
 		LogFile() << xTRACE_VAR(ps1);
 
 		Console console;
-		console.setTitle(title);
-		console.writeLine(ps1);
+		console.setTitle( ::wrapAnsiForPS1(title) );
+		console.writeLine( ::wrapAnsiForPS1(ps1) );
 	}
 
 	return ExitCode::Success;
