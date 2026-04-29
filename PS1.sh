dc2e52be90ff6efc58b4478c040266755306df7d
skynowa <skynowa@gmail.com>
2026-04-29 18:54:29 +0300

    py-impl: PS1.sh [7+/2-] - impl

--- PS1.sh
+++ PS1.sh
@@ -5,9 +5,14 @@
 #
 
 
-function update_ps1()
+function update_ps1_cpp()
+{
+	export PS1="$(~/Projects/GitPrompt_build/GitPrompt $?)"
+}
+
+function update_ps1_py()
 {
 	export PS1="$(python3 ~/Projects/GitPrompt/GitPromptApp.py $?)"
 }
 
-PROMPT_COMMAND=update_ps1
+PROMPT_COMMAND=update_ps1_py
